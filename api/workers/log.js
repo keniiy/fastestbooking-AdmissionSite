f32ed6f2fbde2d0084fba5657fc95888555eb7ca
keniiy <kehindekehinde894@gmail.com>
2021-05-27 01:47:47 +0100

    finshed app

--- api/workers/log.js
+++ api/workers/log.js
@@ -6,7 +6,7 @@ const models = require('../../database/models');
 async function CreateApplicationLogs(job, done) {
   try {
     const { data } = job;
-    const autoCreateApplication = await models.Wallet.create({
+    const autoCreateApplication = await models.Student.create({
       StudentId: data.id,
       program: data.program,
       semester: data.semester,
